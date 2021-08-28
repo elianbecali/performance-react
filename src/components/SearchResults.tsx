179929add0958a6d7365c57ce44199d51df0a1e6
Elian Becali <elianbecaliaguiar@gmail.com>
2021-08-28 08:19:38 -0400

    class about virtualization

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { List, ListRowRenderer } from "react-virtualized";
 
 import { ProductItem } from "./ProductItem";
 
@@ -15,19 +15,29 @@ interface SearchResultsProps {
 
 export function SearchResults({ totalPrice, results, onAddToWishList }: SearchResultsProps) {
 
+  const rowRenderer: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={key} style={style}>
+        <ProductItem
+          product={results[index]}
+          onAddToWishList={onAddToWishList}
+        />
+      </div>
+    );
+  };
+
   return (
     <div>
       <h2>{totalPrice}</h2>
 
-      {results.map(product => {
-        return (
-          <ProductItem
-            key={product.id}
-            product={product}
-            onAddToWishList={onAddToWishList}
-          />
-        );
-      })}
+      <List
+        height={300}
+        rowHeight={100}
+        width={900}
+        overscanRowCount={5}
+        rowCount={results.length}
+        rowRenderer={rowRenderer}
+      />
     </div>
   );
 }
@@ -38,4 +48,10 @@ export function SearchResults({ totalPrice, results, onAddToWishList }: SearchRe
  * 
  * 1. Heavy calculation
  * 2. Reference equality (when passing information to the child component)
+ */
+
+/**
+ * Situations where you should use "virtualization"
+ * 
+ * 1. React components for efficiently rendering large lists and tabular data.
  */
